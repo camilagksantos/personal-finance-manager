a163b8d8ba3474ff3f20fbae60bb51217c5fd325
Camila Kfouri <kfouri.camila@gmail.com>
2026-02-27 09:51:22 +0000

    chore: add Spring Boot project scaffold to backend

---
+++ backend/src/test/java/com/camilagksantos/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camilagksantos.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
